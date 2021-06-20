761bad5bc5a1c9100ca601d8527cf435fd2ed8e5
slatzz <kllanosopspark@gmail.com>
2021-06-20 16:12:33 -0500

    install material ui and react-router. setup homepage.

---
+++ spooky-skeleton/src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Grid, Typography } from '@material-ui/core'
+
+const Homepage = () => {
+    return (
+        <Grid container>
+            <Typography variant="overline">Henlo World</Typography>
+        </Grid>
+    )
+}
+
+export default Homepage
